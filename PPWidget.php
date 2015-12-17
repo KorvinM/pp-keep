10f077ec272fed93cf7d4e3967e4d204acdbb7c0
KorvinM <km@korvin.org>
2015-12-17 00:40:09 +0000

    bugfix

--- PPWidget.php
+++ PPWidget.php
@@ -69,7 +69,7 @@ if (!class_exists('kvnPPWidget')){
 			?>
 			<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $widget_title ); ?>" />
 			</p>
 			<?php 
 		}
