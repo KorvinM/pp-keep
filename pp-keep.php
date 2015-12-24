480a0c6149b07c118312a693a40241e1c5f69b22
KorvinM <km@korvin.org>
2015-12-24 05:26:22 +0000

    separate shotcode inot file

--- pp-keep.php
+++ pp-keep.php
@@ -11,33 +11,7 @@ License: GPL2
 defined( 'ABSPATH' ) || exit;
 define( 'PPKEEP_PATH', plugin_dir_path( __FILE__ ) );
  
-function ppkeep_shortcode_return() {
-	 ob_start();
-	$pp_posts=array(
-		'posts_per_page' => 2,
-		'post_type'=>'post',
-		'post_status'=> 'private',
-	);
-	
-	$query = new WP_Query($pp_posts);
-	if ($query->have_posts() ): ?>
-	<div>
-            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-            <p id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </p>
-            <?php endwhile;
-            wp_reset_postdata(); ?>
-        </div>
-	<?php endif; 
-	wp_reset_postdata();
-	$return_string= ob_get_clean();
-	return $return_string;
-}
-function ppkeep_register_shortcode(){
-	add_shortcode('private-posts', 'ppkeep_shortcode_return');
-}
-add_action('init','ppkeep_register_shortcode');
+require_once( __DIR__ . '/pp-keep-shortcode.php' );
 require_once( __DIR__ . '/class-pp-keep.php' );
 require_once( __DIR__ . '/class-pp-keep-widget.php' );
 if( class_exists( 'PP_Keep' ) ) {
